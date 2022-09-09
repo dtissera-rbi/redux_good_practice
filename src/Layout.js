c766f261239aa8f4bdcf2532f7eb216af6c502de
David Alejandro Tissera <davidtissera94@gmail.com>
2022-09-09 16:29:27 -0300

    configureStore

--- src/Layout.js
+++ src/Layout.js
@@ -1,37 +1,37 @@
 import { useNavigate } from "react-router-dom"
 
 function Layout(props) {
-    const navigate = useNavigate()
-    const menu = [
-        'home',
-        'navigation',
-        'products',
-        'login'
-    ]
-    
-    const handleClickMenuItem = (menuItemName) => {
-        navigate(`/${menuItemName}`)
-    }
-    
-    return (
-        <div>
-            <div>
-                {props.children}
-            </div>
-            <div>
-                {menu.map((menuItem) => {
-                    return (
-                        <button
-                            key={menuItem}
-                            onClick={() => handleClickMenuItem(menuItem)}
-                        >
-                            {menuItem}
-                        </button>
-                    )
-                })}
-            </div>
-        </div>
-    )
+  const navigate = useNavigate()
+  const menu = [
+    { label: 'home', path: '/' },
+    { label: 'navigation', path: '/navigation' },
+    { label: 'products', path: '/products' },
+    { label: 'login', path: '/login' },
+  ]
+
+  const handleClickMenuItem = (path) => {
+    navigate(path)
+  }
+
+  return (
+    <div>
+      <div>
+        {props.children}
+      </div>
+      <div>
+        {menu.map((menuItem) => {
+          return (
+            <button
+              key={menuItem.path}
+              onClick={() => handleClickMenuItem(menuItem.path)}
+            >
+              {menuItem.label}
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Layout
