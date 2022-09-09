0807473639029ed6f83e0a596b65e914d5799260
David Alejandro Tissera <davidtissera94@gmail.com>
2022-09-09 13:39:30 -0300

    update

--- src/views/Products/index.js
+++ src/views/Products/index.js
@@ -1,13 +1,8 @@
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import store from './store'
 import ProductsList from './ProductsList'
 
 export default function ProductsView() {
-  const state = useSelector(state => state)
-
-  console.log('state from store.getState()', store.getState())
-  console.log('state from useSelector', state)
-
   return (
     <Provider store={store}>
       <h1>Products View</h1>
